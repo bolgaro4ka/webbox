5d838b9ead344b5b6f0ec66bdc658f09f31daf6b
Nikita <fedosovnikita007@gmail.com>
2024-06-11 19:37:52 +0300

    МАМА ПОЧЕМУ Я ФРОНТЕНДЕР?

---
+++ webbox_bot/main.py
@@ -0,0 +1,26 @@
+
+from aiogram import Dispatcher, Bot, types
+from aiogram.types.web_app_info import WebAppInfo
+from aiogram.filters.command import Command
+import asyncio
+import logging
+
+bot = Bot(token='7248104392:AAFR4NDI3fnwZ6VVpx54KxOBq2A0EH4K-SY')
+logging.basicConfig(level=logging.INFO)
+dp = Dispatcher()
+
+
+@dp.message(Command('start'))
+async def cmd_start(msg: types.Message):
+    markup = types.ReplyKeyboardMarkup(keyboard=[[types.KeyboardButton(text="Открыть", web_app=WebAppInfo(url="http://localhost:3001"))]])
+    await msg.answer("Привет!", reply_markup=markup)
+
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
