c990906ee19c8cd26b3ed3559c3c619d05277469
Nikita <fedosovnikita007@gmail.com>
2024-06-13 19:13:00 +0300

    Crazy

--- webbox/webbox/telegram/urls.py
+++ webbox/webbox/telegram/urls.py
@@ -6,5 +6,10 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('money/<int:cid>/', views.pay, name='pay')
+    path('money/<int:cid>/', views.pay, name='pay'),
+    path('l/', views.login, name="login"),
+    path('r/', views.register, name='req'),
+    path('a/', views.lessions, name="lessions"),
+    path('a/<int:id>/', views.article, name="article"),
+    path('u/', views.user_unlogin, name='unlogin'),
 ]
--- webbox/webbox/telegram/views.py
+++ webbox/webbox/telegram/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from main.models import Course
 from .forms import TelegramGenericForm
+from account.forms import LoginForm, UserRegistrationForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+from django.contrib.auth import logout
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.clickjacking import xframe_options_exempt
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.db.models import Count
+from django.http import HttpResponse
+from lessions.models import Lession
 
 # Create your views here.
 
@@ -17,3 +28,57 @@ def pay(request, cid):
     else:
         form = TelegramGenericForm()
     return render(request, 'telegram/payment.html', {'cid': cid, 'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect("/")
+
+def user_login(request):
+    if request.method == 'POST':
+        error={}
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect("/u")
+                else:
+                    
+                    error["name"]="Disabled account"
+                    error["description"] = "Пользователь заблокирован. Обратитесь к администратору"
+
+                    return HttpResponse('Disabled account')
+            else:
+                error["name"] = "Invalid login"
+                error["description"] = "Неверное имя пользователя или пароль"
+                return render(request, 'errors_form.html', {'error': error})
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
+@login_required(login_url='/t/l/')
+def article(request, id):
+   
+    return render(request, 'telegram/article.html', {'id': id})
+
+@login_required(login_url='/t/l/')
+def lessions(request):
+    lessions = Lession.objects.all().filter(short_name=request.user.usercourses.course.short_name)
+    return render(request, 'telegram/lessions.html', {'lessions': lessions})
