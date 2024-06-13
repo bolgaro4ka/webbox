3af4991a42c968328ff8dff632596cfe525cece9
Nikita <fedosovnikita007@gmail.com>
2024-06-13 12:01:54 +0300

    Капец, как в шк удобно делать эту хрень

--- webbox/webbox/main/forms.py
+++ webbox/webbox/main/forms.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import User
 
 class Filter(forms.Form):
     field_search = forms.CharField(max_length=255, required=False, label='Поиск')
-    price_from = forms.IntegerField(label="Цена от", required=False)
-    price_to = forms.IntegerField(label="до", required=False)
+    price_from = forms.IntegerField(label="От", required=False)
+    price_to = forms.IntegerField(label="До", required=False)
