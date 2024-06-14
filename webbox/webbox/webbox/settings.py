b83b481f7eeb3f38fc218896253972131185aef4
Nikita <fedosovnikita007@gmail.com>
2024-06-14 13:15:16 +0300

    New apperanse for lession

--- webbox/webbox/webbox/settings.py
+++ webbox/webbox/webbox/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -149,3 +149,6 @@ PUBLIC_ROOT = os.path.join(BASE_DIR, '/media/')
 
 # Путь хранения картинок
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+STATIC_URL = "/static/"
+
